659cf84d4ee5da744327394391f847fb4bba853c
Truong <duytruong5437@gmail.com>
2022-03-27 01:24:39 +0700

    fix search bar

--- src/components/Header/SearchBar.js
+++ src/components/Header/SearchBar.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {useSelector, useDispatch} from "react-redux";
+import {getCourseAll} from '../../actions/CourseAll';
 import {Divider} from "@material-ui/core";
 import { alpha, makeStyles } from "@material-ui/core/styles";
 import InputBase from "@material-ui/core/InputBase";
@@ -8,6 +10,7 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
+import {Link} from 'react-router-dom';
 
 const useStyles = makeStyles((theme) =>({
   search:{
@@ -49,10 +52,41 @@ const useStyles = makeStyles((theme) =>({
         width: "28ch",
       },
     },
-  }
+  },
+  searchResult: {
+    position: "absolute",
+    top: 40,
+    left: 0,
+    width: "100%",
+    maxHeight: "40vh",
+    overflowY: "auto",
+    backgroundColor: theme.palette.background.paper,
+  },
+  link: {
+    textDecoration: "none",
+    textTransform: "none",
+    color: "darkgray",
+  },
 }))
 
 const SearchBar = () => {
+  const {courseAll, loading} = useSelector((state) => state.courseAll);
+  const [keyWord, setKeyWord] = useState(null);
+  const [show, setShow] = useState(false);
+
+  const dispatch = useDispatch();
+
+  useEffect(() =>{
+      dispatch(getCourseAll("all", "GP08", keyWord))
+  },[getCourseAll, keyWord])
+
+  const handleKeyWord = (key) =>{
+     setTimeout(() =>{
+        setKeyWord(key);
+        setShow(true);
+     }, 2000)
+  }
+
   const classes = useStyles();
   return (
     <div className={classes.search}>
@@ -67,7 +101,31 @@ const SearchBar = () => {
                input: classes.inputContent
            }}
            inputProps={{ "aria-label": "search" }}
-        />   
+           onChange={(e) => handleKeyWord(e.target.value)}
+        />
+      {show && keyWord && courseAll && courseAll.length > 0 ? (
+        <List className={classes.searchResult}>
+          {courseAll.map((course) => (
+            <Link
+              key={course.maKhoaHoc}
+              to={`/courses/${course.maKhoaHoc}`}
+              className={classes.link}
+              onClick={() => setShow(false)}
+            >
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <img src={course.hinhAnh} alt={course.tenKhoaHoc} />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={course.tenKhoaHoc} />
+              </ListItem>
+              <Divider />
+            </Link>
+          ))}
+        </List>
+      ) : null}
+           
     </div>
   )
 }
